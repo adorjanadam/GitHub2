815c5779ffda0f9e76a73667cb17f3053abcb99d
Adam <adorjanadam@users.noreply.github.com>
2017-07-20 11:27:13 +0200

    default button color for Snackbar changed

--- app/src/main/java/hu/adamdev/adamadorjan/github2/MainActivity.java
+++ app/src/main/java/hu/adamdev/adamadorjan/github2/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         color = Color.YELLOW;
         break;
       default:
-        color = Color.WHITE;
+        color = Color.BLUE;
         break;
     }
 
